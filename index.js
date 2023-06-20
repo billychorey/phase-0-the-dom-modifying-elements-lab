fbe600f18dbdb108e44f574aca291d98094b5e67
billy chorey <billychorey@gmail.com>
2023-06-20 15:12:08 -0400

    adds dom manipulation exercise

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code here!
+
+
+const main = document.getElementById("main");
+
+ 
+
+const newHeader = document.createElement("h1");
+newHeader.id="victory";
+newHeader.textContent="Billy is the champion";
+main.remove();
+document.body.append(newHeader);
+
